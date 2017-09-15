d3167dcfcb141909731428dacde37d09bc780456
Michael Ginart <michael.ginart@gmail.com>
2017-09-15 07:57:19 +0200

    Adding working project reference xml parser - in heavy need of refactoring

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSearchFolderPath = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtReferences = new System.Windows.Forms.TextBox();
+            this.txtFilesSearched = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(763, 89);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get References from folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSearchFolderPath
+            // 
+            this.txtSearchFolderPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearchFolderPath.Location = new System.Drawing.Point(12, 12);
+            this.txtSearchFolderPath.Name = "txtSearchFolderPath";
+            this.txtSearchFolderPath.Size = new System.Drawing.Size(763, 20);
+            this.txtSearchFolderPath.TabIndex = 2;
+            this.txtSearchFolderPath.Text = "C:\\tfs\\NCSCross\\Release\\5.8\\Hotfix";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 133);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtReferences);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtFilesSearched);
+            this.splitContainer1.Size = new System.Drawing.Size(763, 410);
+            this.splitContainer1.SplitterDistance = 546;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // txtReferences
+            // 
+            this.txtReferences.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtReferences.Location = new System.Drawing.Point(0, 0);
+            this.txtReferences.Multiline = true;
+            this.txtReferences.Name = "txtReferences";
+            this.txtReferences.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtReferences.Size = new System.Drawing.Size(546, 410);
+            this.txtReferences.TabIndex = 2;
+            // 
+            // txtFilesSearched
+            // 
+            this.txtFilesSearched.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtFilesSearched.Location = new System.Drawing.Point(0, 0);
+            this.txtFilesSearched.Multiline = true;
+            this.txtFilesSearched.Name = "txtFilesSearched";
+            this.txtFilesSearched.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtFilesSearched.Size = new System.Drawing.Size(213, 410);
+            this.txtFilesSearched.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 555);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.txtSearchFolderPath);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "List C# project references";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSearchFolderPath;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtReferences;
+        private System.Windows.Forms.TextBox txtFilesSearched;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            readAllFiles();
+        }
+
+        private void readAllFiles() {
+
+            string output = "";
+            string filePaths = "";
+            string[] files = Directory.GetFiles(txtSearchFolderPath.Text, "*.*csproj", SearchOption.AllDirectories);
+
+            //GET REFERENCES
+            foreach (var filePath in files)
+            {
+                filePaths += filePath + System.Environment.NewLine;
+                output += GetReferencesFromFile(filePath); 
+            }
+
+            //OUTPUT
+            txtReferences.Text = "<References>" + System.Environment.NewLine + output + System.Environment.NewLine + "</References>";
+            txtFilesSearched.Text = "Searched in follwing files:" + System.Environment.NewLine + filePaths;
+
+        }
+        private string GetReferencesFromFile(string filePath) {
+
+                 
+            string output = "";
+            XDocument xml = XDocument.Load(filePath);
+
+            //1. Iterate all child elements of <Project> to find <ItemGroup>
+            foreach (XElement projectChildElement in xml.Elements().First().Elements())
+            {
+                //2. If ItemGroup then look for entrys of <Reference> element
+                if (projectChildElement.Name.LocalName == "ItemGroup") {
+                    if (!projectChildElement.HasElements) break;
+                    
+                    
+                    //=&= TODO: Finns riks för logiskt fel här, Reference behöver ej komma först eller vara default, det kan ligga andra element före och då missar man reference element ur en projektfil.
+                    if (projectChildElement.Elements().FirstOrDefault().Name.LocalName == "Reference")
+                    {
+
+
+                        //3. When <Reference> child elements are found in <ItemGroup>, iterate and build up output.
+                        IEnumerable<XElement>itemGroup = projectChildElement.Elements();
+                        Reference refObj = new Reference();
+                        
+                        foreach (XElement referenceElement in itemGroup)
+                        {
+                            refObj = LoadReferenceObject(referenceElement, filePath);
+                            output += @"<Reference " + refObj.ReferenceInformation 
+                                    + @" SpecificVersion=""" + refObj.SpecificVersion 
+                                    + @""" HintPath=""" + refObj.HintPath 
+                                    + @""" Private=""" + refObj.Private 
+                                    + @""" ExtDep=""" + refObj.ExternalDependency 
+                                    + @""" InOutputFolder=""" + refObj.InOutputFolder 
+                                    + @""" InPkgFolder=""" + refObj.InPackagesFolder 
+                                    + @""" InProject=""" + refObj.ProjectPath 
+                                    + @"""></Reference>" 
+                                    + System.Environment.NewLine;
+
+                        }
+                        
+                    }
+                }
+            }
+            return output;
+                        
+        }
+
+        private Reference LoadReferenceObject(XElement reference, string filePath)
+        {
+            //Assign value to InfoObject
+            Reference refObj = new Reference();
+            refObj.ReferenceInformation = reference.Attribute("Include").ToString();
+            refObj.ProjectPath = filePath;
+            foreach (XElement referenceChild in reference.Elements())
+            {
+                switch (referenceChild.Name.LocalName)
+                {
+                    case "HintPath":
+                        refObj.HintPath = referenceChild.Value;
+
+                        if (referenceChild.Value.ToString().Contains(@"\ExternalDep\"))
+                        {
+                            refObj.ExternalDependency = "True";
+                        }
+                        else
+                        {
+                            refObj.ExternalDependency = "False";
+                        }
+
+                        if (referenceChild.Value.ToString().Contains(@"\Output\"))
+                        {
+                            refObj.InOutputFolder = "True";
+                        }
+                        else
+                        {
+                            refObj.InOutputFolder = "False";
+                        }
+                        if (referenceChild.Value.ToString().Contains(@"\packages\"))
+                        {
+                            refObj.InPackagesFolder = "True";
+                        }
+                        else
+                        {
+                            refObj.InPackagesFolder = "False";
+                        }
+
+
+                        break;
+                    case "SpecificVersion":
+                        refObj.SpecificVersion = referenceChild.Value;
+                        break;
+                    case "Private":
+                        refObj.Private = referenceChild.Value.ToString();
+                        break;
+
+                    default:
+                        refObj.AdditionalInformation += referenceChild.Value;
+                        break;
+                }
+
+            }
+            return refObj;
+        }
+    }
+}
+public class Reference{
+
+    public string ProjectPath { get; set; }
+
+    public string SpecificVersion { get; set; }
+    public string HintPath { get; set; }
+    public string Private { get; set; }
+    public string ReferenceInformation { get; set; }
+    public string AdditionalInformation { get; set; }
+    public string ExternalDependency { get; set; }
+    public string InOutputFolder { get; set; }
+    public string InPackagesFolder { get; set; }
+}
